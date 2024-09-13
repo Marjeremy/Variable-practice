92ef74737a6cba191934b358912ea5de116dd783
Marjeremy <gaoat856@gmail.com>
2024-09-13 20:15:02 +0800

    Variable-practice commit

---
+++ variable.js
@@ -0,0 +1,9 @@
+let name = "Mark Jeremy Gaoat ";
+let WhatDoYouWannaBecomeInYourLife = "programmer ";
+let gender = "male";
+let twitterhandel = "@Markjeremy24";
+
+console.log("Name:", name);
+console.log("desired career :", WhatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter handle;", twitterhandel);
